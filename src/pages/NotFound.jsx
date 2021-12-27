4a9bda37cdc1379f94e4f10f97317685f1b7e733
carlosmen <carlosamenesesc@gmail.com>
2021-12-26 18:40:28 -0600

    Creating the full base for the project

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoyFound = () => {
+  return (
+    <div>
+      <img src="https://dam.muyinteresante.com.mx/wp-content/uploads/2020/04/error-404-770x513.jpg" alt="" />
+    </div>
+  )
+}
+
+export default NoyFound
